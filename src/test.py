64628ab28177359206813b481bf447f55708ef0d
Emre Rasimgil <erasimgil@gmail.com>
2024-07-18 10:22:56 +0200

    bug fix

--- src/test.py
+++ src/test.py
@@ -1,7 +1,13 @@
 import mido 
+import os
 
-mid = mido.MidiFile("./aux_data/Chromatic_scale_ascending_on_C.mid")
-mid1 = mido.MidiFile("./aux_data/Chromatic_scale_descending_on_C.mid")
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+
+data_dir = os.path.join(root_dir, "midi-emotion", "src", "aux_data", )
+mid_path = os.path.join(data_dir, "Chromatic_scale_ascending_on_C.mid")
+mid1_path = os.path.join(data_dir, "Chromatic_scale_descending_on_C.mid")
+mid = mido.MidiFile(mid_path)
+mid1 = mido.MidiFile(mid1_path)
 
 outport = mido.open_output(mido.get_output_names()[2])
 
